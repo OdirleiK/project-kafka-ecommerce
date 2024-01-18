808e8509b82f602abae64487333b291890c5c41c
OdirleiK <odirlei67777@gmail.com>
2024-01-18 08:27:34 -0300

    :tada: Starting project

---
+++ project-kafka-ecommerce/src/test/java/br/com/kmpx/projectkafkaecommerce/ProjectKafkaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kmpx.projectkafkaecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKafkaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
