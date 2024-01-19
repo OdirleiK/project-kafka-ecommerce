f9f450bdad7f46ec985b70a2cb6ed08672647218
OdirleiK <odirlei67777@gmail.com>
2024-01-19 18:19:55 -0300

    :sparkles: feat: created EmailService and LogService consumers

--- project-kafka-ecommerce/src/main/java/br/com/kmpx/projectkafkaecommerce/ProjectKafkaEcommerceApplication.java
+++ project-kafka-ecommerce/src/main/java/br/com/kmpx/projectkafkaecommerce/ProjectKafkaEcommerceApplication.java
@@ -4,6 +4,7 @@ import java.security.Timestamp;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -16,14 +17,24 @@ public class ProjectKafkaEcommerceApplication {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		var producer = new KafkaProducer<String, String>(properties());
 		var value = "9x32123,8989,45321";
+		var email = "Thank you for your order! We are processing your order!";
+		
 		var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-		producer.send(record, (data, ex) -> {
+		var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+		
+		
+		Callback callback = (data, ex) -> {
 			if(ex != null) {
 				ex.printStackTrace();
 				return;
 			}
 			System.out.println("success sending: " + data.topic() + ":::partition" + data.partition() + "/ offset" + data.offset() + "/ timestamp" + data.timestamp());	
-		}).get();
+		};
+		
+		
+		
+		producer.send(record, callback).get();
+		producer.send(emailRecord, callback).get();
 	}
 
 	private static Properties properties() {
