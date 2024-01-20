d838a165d6bd405cf29f90a72a4b4ea70af5df55
OdirleiK <odirlei67777@gmail.com>
2024-01-20 19:33:17 -0300

    :sparkles: feat: Serialization with gson

--- project-kafka-ecommerce/src/main/java/br/com/kmpx/projectkafkaecommerce/ProjectKafkaEcommerceApplication.java
+++ project-kafka-ecommerce/src/main/java/br/com/kmpx/projectkafkaecommerce/ProjectKafkaEcommerceApplication.java
@@ -1,5 +1,6 @@
 package br.com.kmpx.projectkafkaecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -9,16 +10,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectKafkaEcommerceApplication {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		try(var dispatcher = new KafkaDispatcher()) {
-			var key = UUID.randomUUID().toString();
-			var value = key + ",8989,45321";
-			
-			dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
-	
-			var email = "Thank you for your order! We are processing your order!";
-			dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+		try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+			try (var emailDispatcher = new KafkaDispatcher<String>()) {
+
+				var userId = UUID.randomUUID().toString();
+				var orderId = UUID.randomUUID().toString();
+				var amount = new BigDecimal(Math.random() * 5000 + 1);
+				var order = new Order(userId, orderId, amount);
+
+				orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+				var email = "Thank you for your order! We are processing your order!";
+				emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+			}
 		}
-		
+
 	}
 
 }
