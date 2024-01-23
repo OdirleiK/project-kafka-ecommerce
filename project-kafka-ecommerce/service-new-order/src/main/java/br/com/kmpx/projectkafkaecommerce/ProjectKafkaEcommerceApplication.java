b4f89d73a76bf587f3532d59573ce69ace4ba2a6
OdirleiK <odirlei67777@gmail.com>
2024-01-23 19:37:35 -0300

    :recycle: refactor: Decoupling modules

--- project-kafka-ecommerce/service-new-order/src/main/java/br/com/kmpx/projectkafkaecommerce/ProjectKafkaEcommerceApplication.java
+++ project-kafka-ecommerce/service-new-order/src/main/java/br/com/kmpx/projectkafkaecommerce/ProjectKafkaEcommerceApplication.java
@@ -11,13 +11,13 @@ public class ProjectKafkaEcommerceApplication {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		try (var orderDispatcher = new KafkaDispatcher<Order>()) {
-			try (var emailDispatcher = new KafkaDispatcher<Email>()) {
+			try (var emailDispatcher = new KafkaDispatcher<String>()) {
 				for(var i = 0; i < 10; i++) {
 					var userId = UUID.randomUUID().toString();
 					var orderId = UUID.randomUUID().toString();
 					var amount = new BigDecimal(Math.random() * 5000 + 1);
 					var order = new Order(userId, orderId, amount);
-					var email = new Email("processing your order", "Thank you for your order! We are processing your order!");
+					var email = "processing your order Thank you for your order! We are processing your order!";
 					
 					//var email = "Thank you for your order! We are processing your order!";
 					orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
