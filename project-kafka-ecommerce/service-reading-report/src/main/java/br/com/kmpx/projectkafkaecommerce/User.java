0eea4e997d760fada6b13c0371c55c4761284455
OdirleiK <odirlei67777@gmail.com>
2024-01-29 20:03:26 -0300

    :sparkles: feat: Created service reading report

---
+++ project-kafka-ecommerce/service-reading-report/src/main/java/br/com/kmpx/projectkafkaecommerce/User.java
@@ -0,0 +1,20 @@
+package br.com.kmpx.projectkafkaecommerce;
+
+public class User {
+
+	private final String uuid;
+
+	public User(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getReportPath() {
+		return "target/" + uuid + "-report.txt";
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+	
+	
+}
