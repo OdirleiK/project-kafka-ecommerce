a795bc3bc973b6deda9be26869289d7337b5be16
OdirleiK <odirlei67777@gmail.com>
2024-02-02 18:10:18 -0300

    :sparkles: feat: Created send async method in dispatcher

--- project-kafka-ecommerce/commom-kafka/src/main/java/br/com/kmpx/projectkafkaecommerce/KafkaDispatcher.java
+++ project-kafka-ecommerce/commom-kafka/src/main/java/br/com/kmpx/projectkafkaecommerce/KafkaDispatcher.java
@@ -3,11 +3,13 @@ package br.com.kmpx.projectkafkaecommerce;
 import java.io.Closeable;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 class KafkaDispatcher<T> implements Closeable{
@@ -30,6 +32,12 @@ class KafkaDispatcher<T> implements Closeable{
 	}
 
 	void send(String topic, String key, CorrelationId id, T payload) throws InterruptedException, ExecutionException {
+		var future = sendAsync(topic, key, id, payload);
+		future.get();
+	}
+
+
+	private Future<RecordMetadata> sendAsync(String topic, String key, CorrelationId id, T payload) {
 		var value = new Message<T>(id, payload);
 		var record = new ProducerRecord<>(topic, key, value);
 
@@ -41,7 +49,8 @@ class KafkaDispatcher<T> implements Closeable{
 			System.out.println("success sending: " + data.topic() + ":::partition" + data.partition() + "/ offset" + data.offset() + "/ timestamp" + data.timestamp());	
 		};
 		
-		producer.send(record, callback).get();
+		var future = producer.send(record, callback);
+		return future;
 	}
 
 	@Override
